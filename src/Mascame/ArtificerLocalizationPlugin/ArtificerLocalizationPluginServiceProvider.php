4d07206b472cbd5d2b8b0b67ddf2ca22e3a9df4c
Marc Mascarell <marcmascarell@gmail.com>
2016-11-01 21:53:48 +0000

    Applied fixes from StyleCI

--- src/Mascame/ArtificerLocalizationPlugin/ArtificerLocalizationPluginServiceProvider.php
+++ src/Mascame/ArtificerLocalizationPlugin/ArtificerLocalizationPluginServiceProvider.php
@@ -1,40 +1,41 @@
-<?php namespace Mascame\ArtificerLocalizationPlugin;
+<?php
+
+namespace Mascame\ArtificerLocalizationPlugin;
 
 use Illuminate\Support\ServiceProvider;
 use Mascame\Artificer\Plugin\PluginManager;
-use Route;
 use App;
 
-class ArtificerLocalizationPluginServiceProvider extends ServiceProvider {
-
+class ArtificerLocalizationPluginServiceProvider extends ServiceProvider
+{
     protected $package = 'mascame/artificer-localization-plugin';
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		$this->package($this->package);
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->package($this->package);
 
         $this->addPublishCommand();
-	}
+    }
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
         $package = $this->package;
 
         App::bind($this->package, function () use ($package) {
@@ -42,11 +43,11 @@ class ArtificerLocalizationPluginServiceProvider extends ServiceProvider {
         });
 
         new PluginManager($this->package);
-	}
+    }
 
     private function addPublishCommand()
     {
-        $command_key = $this->package . '-command-publish';
+        $command_key = $this->package.'-command-publish';
 
         App::bind($command_key, function () {
             return new PublishCommand();
@@ -55,14 +56,13 @@ class ArtificerLocalizationPluginServiceProvider extends ServiceProvider {
         $this->commands($command_key);
     }
 
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array();
-	}
-
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [];
+    }
 }
